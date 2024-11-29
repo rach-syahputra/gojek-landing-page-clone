bc4f01a80d5859781b3535986cab9e3f6e30b7be
rach-syahputra <rach.syahputra@gmail.com>
2024-11-29 10:24:54 +0800

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
       },
-    },
+      fontFamily: {
+        MaisonNeueBold: ['--font-maison-neue-bold'],
+        MaisonNeueBook: ['--font-maison-neue-book'],
+        MaisonNeueDemi: ['--font-maison-neue-demi'],
+        MaisonNeueExtendedBold: ['--font-maison-neue-extended-bold']
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
