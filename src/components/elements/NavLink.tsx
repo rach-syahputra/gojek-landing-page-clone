ac758f193eee5ea617a1758b65f9ad2ca12202ba
rach-syahputra <rach.syahputra@gmail.com>
2024-11-29 13:16:30 +0800

    replace NavLink anchor tag with Link component

--- src/components/elements/NavLink.tsx
+++ src/components/elements/NavLink.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function NavLink({
   children,
@@ -9,7 +10,7 @@ export default function NavLink({
 }) {
   return (
     <li className='font-MaisonNeueDemi flex gap-2 text-lg'>
-      <a href='#'>{children}</a>
+      <Link href='#'>{children}</Link>
       {dropdown && (
         <Image src='/arrow-down.svg' width={16} height={16} alt='arrow down' />
       )}
