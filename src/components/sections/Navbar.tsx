f1d6691cad088a1ae4e98d2c038a4405060b6b1a
rach-syahputra <rach.syahputra@gmail.com>
2024-11-29 16:46:13 +0800

    implement sticky navbar

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -4,7 +4,7 @@ import Hamburger from '../elements/hamburger'
 
 export default function Navbar() {
   return (
-    <nav className='flex h-20 items-center justify-between bg-background px-10 text-white lg:h-[100px]'>
+    <nav className='sticky top-0 z-20 flex h-20 w-full items-center justify-between bg-background px-10 text-white lg:h-[100px]'>
       <div className='relative h-full w-[100px] xl:w-[112px]'>
         <Image src='/logo.svg' alt='gojek logo' fill />
       </div>
